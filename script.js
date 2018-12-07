ba5bbc095c47455f183ad1c1e9274ea8ac1e13fa
Gabriel Hazi <hazigabriel@gmail.com>
2018-12-07 13:52:14 +0200

    Finished the grid generator, now every div is resised to fit the grid

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let userSubmit = document.getElementById("userSubmit");
 let resetGrid = document.getElementById("resetGrid");
 let userInput = document.getElementById("userInput");
 let grid = document.getElementById("grid");
+let gridBlock = document.getElementById("grid").child;
 
 
 userSubmit.onclick = function(){ 
@@ -12,7 +13,24 @@ userSubmit.onclick = function(){
 	for(i = 0; i < (userInput.value * userInput.value) ; i++) {
 		const gridBlock = document.createElement("div");
 		grid.appendChild(gridBlock);
-		grid.style.grid-template-columns = "repeat(auto-fill, minmax(300px,1fr)";
-	}
+    }
+		gridBlockSize = 600 / userInput.value; /*determine the grid in pixels, to have the grid  blocks generated at that particular size and to
+		 have  them fit the grid*/
+		
+		grid.style.gridTemplate = "repeat(" + userInput.value + ", " + gridBlockSize + "px) / " + "repeat(" + userInput.value + ", " + gridBlockSize + "px) ";
+		/*on the line above we modify the css using javascript variable by concatenating them into the css style string, we use userInput value
+		to set the columns/rows number and gridBlockSize to set the size of each div, to fit the grid*/
+
 
 }
+
+/*
+	var nodes = document.getElementById('grid').childNodes;
+for(var i=0; i<nodes.length; i++) {
+    if (nodes[i].nodeName.toLowerCase() == 'div') {
+         nodes[i].style.background = "red";
+     }
+}
+ //// will use this later on, to select the divs generated and have their color changed
+
+*/
