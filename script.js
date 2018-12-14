da95370bb3f1d3785a2ade668b0e010b862d4ddd
Gabriel Hazi <hazigabriel@gmail.com>
2018-12-14 13:59:46 +0200

    added add random on hover, currently working on generating the monochrome color, by incrementing them so that after a few hovers we'll reach solid black, also need to clean up the code

--- script.js
+++ script.js
@@ -2,35 +2,88 @@ let userSubmit = document.getElementById("userSubmit");
 let resetGrid = document.getElementById("resetGrid");
 let userInput = document.getElementById("userInput");
 let grid = document.getElementById("grid");
-let gridBlock = document.getElementById("grid").child;
+let toggleColor = document.getElementById("toggleColor");
+let gridBlock = document.getElementsByClassName("gridBlock");
+
+
+
+
+for(i = 0; i <= 16*16; i++) {
+const createGrid = document.createElement("div");
+createGrid.className = "gridBlock";
+grid.appendChild(createGrid);
+   };
+
+let generateRandomColor = function(){
+	$(".gridBlock").hover(function(){
+		$(this).css("background-color", "rgb(" + " " + Math.floor(Math.random() *256) + " " + Math.floor(Math.random() *256) + " " + Math.floor(Math.random() *256) + ")")
+	})
+};
+
+let generateMonochrome = function(){
+	$(".gridBlock").hover(function(){
+		$(this).css("background-color", "rgb(0, 0, 0, 0.3)");
+	});
+};
+/*$(".gridBlock").hover(function(){ $(this).css("background-color", "rgb(234 32 132  0.3) ") } )c*/
+
+ generateRandomColor();
+/*create the initial 16*16 grid, and generate a random color by default*/
+	
+
+
 
 
 userSubmit.onclick = function(){ 
-	while(grid.firstChild) {
-		grid.removeChild(grid.firstChild)
-	};  /*this while function removes any child elements of #grid, 
-	so that the grid blocks won't stack up*/
-	for(i = 0; i < (userInput.value * userInput.value) ; i++) {
-		const gridBlock = document.createElement("div");
-		grid.appendChild(gridBlock);
-    }
-		gridBlockSize = 600 / userInput.value; /*determine the grid in pixels, to have the grid  blocks generated at that particular size and to
-		 have  them fit the grid*/
-		
-		grid.style.gridTemplate = "repeat(" + userInput.value + ", " + gridBlockSize + "px) / " + "repeat(" + userInput.value + ", " + gridBlockSize + "px) ";
-		/*on the line above we modify the css using javascript variable by concatenating them into the css style string, we use userInput value
-		to set the columns/rows number and gridBlockSize to set the size of each div, to fit the grid*/
+	if(userInput.value > 64 || isNaN(userInput.value) || userInput.value == 0) {
+		alert("Please try again, and input a number between 1 and 64.");
+	} else { 
+		while(grid.firstChild) {
+			grid.removeChild(grid.firstChild)
+		};  /*this while function removes any child elements of #grid, 
+		so that the grid blocks won't stack up*/
+		for(i = 0; i < (userInput.value * userInput.value) ; i++) {
+			const createGrid = document.createElement("div");
+			createGrid.className = "gridBlock";
+			grid.appendChild(createGrid);
+
+	    }
+			gridBlockSize = 600 / userInput.value; /*determine the grid in pixels, to have the grid  blocks generated at that particular size and to
+			 have  them fit the grid*/
+			
+			grid.style.gridTemplate = "repeat(" + userInput.value + ", " + gridBlockSize + "px) / " + "repeat(" + userInput.value + ", " + gridBlockSize + "px) ";
+			/*on the line above we modify the css using javascript variable by concatenating them into the css style string, we use userInput value
+			to set the columns/rows number and gridBlockSize to set the size of each div, to fit the grid*/
+
+	} 
+
+	generateRandomColor();
+
+};
 
 
-}
+$("#randomColor").click(function(){
+	generateRandomColor();
+});
+$("#monochrome").click(function(){
+	generateMonochrome();
+})
 
+
+$("#resetGrid").click(function(){
+	$(".gridBlock").css("background-color", "rgb(256 256 256)")
+})
 /*
-	var nodes = document.getElementById('grid').childNodes;
-for(var i=0; i<nodes.length; i++) {
-    if (nodes[i].nodeName.toLowerCase() == 'div') {
-         nodes[i].style.background = "red";
-     }
-}
- //// will use this later on, to select the divs generated and have their color changed
+pseudo code
 
+when click "random color" {
+	gridBlock.hover {
+	change color to random
+	}
+}
+when click "monochrome" {
+	gridBlock.hober {
+	add opacity//
+	}
+}
 */
